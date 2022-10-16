679bc39175db1c6ff3e0185ec0c5230047d20598
Guo Yunhe <i@guoyunhe.me>
2022-10-16 22:31:54 +0800

    auth

---
+++ app.js
@@ -0,0 +1,75 @@
+// dependencies
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
+var cors = require('cors');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(flash());
+app.use(passport.session());
+
+app.use('/', routes);
+
+// passport config
+var User = require('./models/User');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const passport = require('passport');
+const User = require('../models/User');
+const router = express.Router();
+
+router.post('/register', (req, res, next) => {
+    User.register(new User({ username : req.body.username }), req.body.password, (err, user) => {
+        if (err) {
+          return res.render('register', { error : err.message });
+        }
+
+        passport.authenticate('local')(req, res, () => {
+            req.session.save((err) => {
+                if (err) {
+                    return next(err);
+                }
+                res.status(200).send(user);
+            });
+        });
+    });
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true }), (req, res, next) => {
+    req.session.save((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.status(200).send(user);
+    });
+});
+
+router.get('/me', (req, res) => {
+    res.status(200).send(req.user);
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    req.session.save((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.status(200).send();
+    });
+});
+
+module.exports = router;
