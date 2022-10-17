7edbcccdbcc132f211e559075e6e9c746a3f7b9c
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2022-10-17 22:07:40 +0800

    db

--- app.js
+++ app.js
@@ -1,77 +1,80 @@
 // dependencies
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var flash = require('connect-flash');
-var cors = require('cors');
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var flash = require("connect-flash");
+var cors = require("cors");
+var routes = require("./routes/index");
+var users = require("./routes/users");
 
 var app = express();
 
 app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('express-session')({
-    secret: 'keyboard cat',
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(flash());
 app.use(passport.session());
 
-app.use('/', routes);
+app.use("/", routes);
 
 // passport config
-var User = require('./models/User');
+var User = require("./models/User");
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// mongoose
-// metro
-// abcd1234
-mongoose.connect('mongodb://metro:abcd1234@metro.cluster-cqjoxidft8d5.us-east-1.docdb.amazonaws.com:27017');
+mongoose.connect(
+  "mongodb://metro:abcd1234@metro2.cluster-cqjoxidft8d5.us-east-1.docdb.amazonaws.com:27017/metro?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+if (app.get("env") === "development") {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err,
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {},
+  });
 });
-
 
 module.exports = app;
